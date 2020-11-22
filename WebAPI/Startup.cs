7e4f615f430e661cd52300d1f5f9ebda188511f1
Fructuoso <victor@fructuoso.com.br>
2020-11-22 20:25:29 -0300

    Implementando Swagger

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,30 +1,32 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
 namespace WebAPI
 {
     public class Startup
     {
+        private readonly IConfiguration _Configuration;
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Fructuoso.Template", Version = "v1" });
+            });
+            #endregion
+
             services.AddControllers();
         }
 
@@ -35,6 +37,15 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            #region Swagger
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
+            });
+            #endregion
 
             app.UseHttpsRedirection();
 
