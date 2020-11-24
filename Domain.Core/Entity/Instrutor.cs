a63222dbe7d2153b2c7335f5e46c9ee3cc72992e
Fructuoso <victor@fructuoso.com.br>
2020-11-23 22:43:16 -0300

    Criando repositório 'Instrutor'

---
+++ Domain.Core/Entity/Instrutor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Fructuoso.Template.Domain.Core.Entity
+{
+    public class Instrutor : BaseEntity<Guid>
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ WebAPI/Controllers/InstrutorController.cs
@@ -0,0 +1,13 @@
+﻿using Fructuoso.Template.Domain.Core.Entity;
+using Fructuoso.Template.Domain.Core.Interfaces.Service;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    public class InstrutorController : GenericControllerCrud<Guid, Instrutor>
+    {
+        public InstrutorController(IServiceCrud<Guid, Instrutor> service) : base(service) { }
+    }
+}
